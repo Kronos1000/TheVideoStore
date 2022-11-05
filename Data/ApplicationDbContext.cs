ed8d577cca3ff8afa399427b1cc1b092f529c4c3
Kronos1000 <101075639+Kronos1000@users.noreply.github.com>
2022-11-05 21:23:08 +1300

    initialize

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using KronosVideo.Models;
+
+namespace KronosVideo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<KronosVideo.Models.Member> Member { get; set; }
+        public DbSet<KronosVideo.Models.Movie> Movie { get; set; }
+        public DbSet<KronosVideo.Models.Rental> Rental { get; set; }
+    }
+}
